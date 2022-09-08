4aa47e156f302297ae25db8e81e36f92e7c57c9c
Frederic Ruelle <frederic.ruelle@st.com>
2022-09-08 14:50:38 +0200

    build: remove non-ST platforms

--- platform/ext/target/arm/corstone1000/ci_regression_tests/s_test_config.cmake
+++
@@ -1,8 +0,0 @@
-#-------------------------------------------------------------------------------
-# Copyright (c) 2021-22, Arm Limited. All rights reserved.
-#
-# SPDX-License-Identifier: BSD-3-Clause
-#
-#-------------------------------------------------------------------------------
-
-############ Define secure test specific cmake configurations here #############
